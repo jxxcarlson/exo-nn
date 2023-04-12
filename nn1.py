17b43ca179422bd6de64d499ddf84d955a3a9183
James Carlson <jxxcarlson@gmail.com>
2023-04-12 16:56:59 +0000

    Add file nn1.py

---
+++ nn1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+def relu(z): # takes a numpy array as input and returns activated array
+    a = np.maximum(0,z)
+    return a
+
+def initialize_params(layer_sizes): #takes a list of the layer sizes as input and returns initialized parameters
+    params = {}
+    for i in range(1, len(layer_sizes)):
+        params['W' + str(i)] = np.random.randn(layer_sizes[i], layer_sizes[i-1])*0.01
+        params['B' + str(i)] = np.random.randn(layer_sizes[i],1)*0.01
+    return params
+
