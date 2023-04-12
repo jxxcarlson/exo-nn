11f664e2c0beaebb4f3273d2fe00f7752befa0a3
James Carlson <jxxcarlson@gmail.com>
2023-04-12 15:16:16 -0400

    Correct imports in nn1.py (scikit-learn vs sklearn mess)

--- nn1.py
+++ nn1.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_boston
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 
